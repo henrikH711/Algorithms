e82ab4669a3469f3882a99ef8a3b40c3f24b2f9f
Henrik <hegyihenrik72@gmail.com>
2025-01-08 20:26:19 +0100

    add

---
+++ rate_monotonic_scheduling.py
@@ -0,0 +1,18 @@
+#!/bin/env/python3
+
+def sort_by_period(process):
+    return process[1]  # Sort by the second element 
+
+def rate_monotonic_scheduling(processes):
+    # Sort processes 
+    processes.sort(key=sort_by_period)
+    
+    for process in processes:
+        print("Running process:", process[0])
+        print("Process", process[0], "has period:", process[1])
+        print("Process", process[0], "finished.")
+
+processes = [("P1", 3), ("P2", 5), ("P3", 1)]  
+
+
+rate_monotonic_scheduling(processes)
